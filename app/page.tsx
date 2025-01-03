814fb8670ce8cd63249e2cbf01a6e596d4051afb
Pham Hoang Minh Anh <minhanh26102004@gmail.com>
2025-01-03 12:46:28 +0700

    Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -10,7 +12,7 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Minh Anh</span>
@@ -43,9 +45,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
