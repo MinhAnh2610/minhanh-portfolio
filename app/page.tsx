dde9d7ef0069914fae5ae9402d7a99f850e510b2
Pham Hoang Minh Anh <minhanh26102004@gmail.com>
2025-01-03 10:52:42 +0700

    Social Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,53 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// components
+import Social from "@/components/Social";
+
+const Home = () => {
   return (
-    <main>Home Page</main>
+    <section className="h-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Minh Anh</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Creative software developer with experience in designing and
+              maintaining scalable web and mobile applications using
+              technologies like [.NET, React]. Passionate about clean code,
+              user-focused solutions, and staying updated with emerging
+              technologies.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant={"outline"}
+                size={"lg"}
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles={"flex gap-6"}
+                  iconStyles={
+                    "w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
---
+++ components/Social.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import path from "path";
+
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icons: <FaGithub />, path: "" },
+  { icons: <FaLinkedinIn />, path: "" },
+  { icons: <FaYoutube />, path: "" },
+  { icons: <FaTwitter />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }: any) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((social, index) => {
+        return (
+          <Link key={index} href={social.path} className={iconStyles}>
+            {social.icons}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
